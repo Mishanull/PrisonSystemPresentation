56ceb82d9ac4e3b53c35d1f21244441a5773cf43
Juraj Sabik <jsb@trifork.com>
2022-05-23 12:54:20 +0200

    removing guards from workshift

--- Contracts/IWorkShiftService.cs
+++ Contracts/IWorkShiftService.cs
@@ -9,4 +9,6 @@ public interface IWorkShiftService
     public Task CreateWorkShiftAsync(WorkShift workShift);
     public Task RemoveWorkShiftAsync(long id);
     public Task UpdateWorkShiftAsync(WorkShift workShift);
+    public Task AddGuardToWorkShift(string guardId, string shiftId);
+    public Task RemoveGuardFromWorkShift(string guardId, string shiftId);
 }
