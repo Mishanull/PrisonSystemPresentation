69bb263ea6158a9281c5f10b0cbff7002be98adf
Benjamín Bartošík <benjamin.bartosik@gmail.com>
2022-05-25 17:16:42 +0200

    prisoner integration tests -part1

---
+++ TestingProject/IntegrationTests/PrisonerIntegrationTests.cs
@@ -0,0 +1,51 @@
+using Contracts;
+using Entities;
+using RabbitMqClients;
+
+namespace TestingProject.IntegrationTests;
+
+public class PrisonerIntegrationTests
+{
+    private IPrisonerService _prisonerService = null!;
+    private ISectorService _sectorService = null!;
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _prisonerService = new PrisonerClient();
+        _sectorService = new SectorClient();
+    }
+
+    private async Task<Prisoner> NewPrisoner()
+    {
+        return new Prisoner
+        {
+            FirstName = "Firstname",
+            LastName = "Lastname",
+            Sector = (await _sectorService.GetSectorsAsync()).FirstOrDefault(),
+            Ssn = 12324354,
+            CrimeCommitted = "crime committed",
+            EntryDate = DateTime.Now,
+            ReleaseDate = DateTime.Now,
+            Notes = new List<Note>()
+        };
+    }
+
+    [Test]
+    [Category("view-prisoner_tests")]
+    public async Task CreatePrisoner_test()
+    {
+        Prisoner p = await NewPrisoner();
+        Assert.DoesNotThrow(() => _prisonerService.CreatePrisonerAsync(p));
+        p = await _prisonerService.GetPrisonerBySSN(p.Ssn.ToString());
+        await _prisonerService.RemovePrisonerAsync(p.Id);
+    }
+    
+    [Test]
+    [Category("view-prisoner_tests")]
+    public async Task GetPrisonerBySsn_test()
+    {
+        var prisoners = await _prisonerService.GetPrisonersAsync(1, 1);
+        Prisoner p = await _prisonerService.GetPrisonerBySSN(prisoners.First().Ssn.ToString());
+        Assert.That(p.Ssn, Is.EqualTo(prisoners.First().Ssn));
+    }
+}
