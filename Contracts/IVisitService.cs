748e7a9e1cd3229398b46e8237b3247716d0f7cd
Mike Abbey <mihaiavram72@gmail.com>
2022-05-26 03:27:35 +0200

    -pagination + other bug fixes

--- Contracts/IVisitService.cs
+++ Contracts/IVisitService.cs
@@ -4,8 +4,8 @@ namespace Contracts;
 
 public interface IVisitService
 {
-    Task CreateVisitAsync(Visit visit);
-    Task<Visit> GetAccessCodeConfirmation(string code);
-    Task UpdateVisitStatusAsync(long id, Status status);
-    Task<ICollection<Visit>> GetVisitsAsync(int pageNumber, int pageSize);
+    public Task CreateVisitAsync(Visit visit);
+    public Task<Visit> GetAccessCodeConfirmation(string code);
+    public Task UpdateVisitStatusAsync(long id, Status status);
+    public Task<ICollection<Visit>> GetVisitsAsync(int pageNumber, int pageSize);
 }
